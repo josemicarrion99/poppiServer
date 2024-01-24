43e2cc28911b713d934fe6447b94a7431947afa1
Jose Miguel <jose@slidescore.com>
2024-01-24 22:09:18 +0100

    Final change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -7,47 +7,41 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-// const dbName = process.env.DATABASE_NAME;
-// const collectionName = "surveyResults";
-
-app.get('/', (req,res) => res.json({message: 'Josee'}))
-app.get('/saveSurvey', (req,res) => res.json({message: 'saveSurvey'}))
-app.post('/saveSurvey', (req,res) => res.json({message: 'postSave'}))
+const dbName = process.env.DATABASE_NAME;
+const collectionName = "surveyResults";
 
 
-// const uri = process.env.AZURE_COSMOS_CONNECTIONSTRING || 'fallback_connection_string';
-// console.log(dbName)
-// console.log(uri)
+const uri = process.env.AZURE_COSMOS_CONNECTIONSTRING || 'fallback_connection_string';
 
-// const client = new MongoClient(uri);
+const client = new MongoClient(uri);
 
-// client.connect()
-//     .then(() => {
-//         console.log('Connected to MongoDB');
-//     })
-//     .catch(err => {
-//         console.error('Error connecting to MongoDB:', err);
-//     });
+client.connect()
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
-// const database = client.db(dbName);
-// const collection = database.collection(collectionName);
+const database = client.db(dbName);
+const collection = database.collection(collectionName);
 
-// app.post('/saveSurvey', async (req, res) => {
-//     console.log(req.body);
+app.post('/saveSurvey', async (req, res) => {
+    console.log(req.body);
 
-//     const surveyData = JSON.parse(req.body.surveyData);
+    const surveyData = JSON.parse(req.body.surveyData);
 
-//     try {
-//         const insertResult = await collection.insertOne(surveyData);
-//         console.log(`Documents successfully inserted.\n`);
-//         res.status(200).json({ message: 'Survey data saved successfully' });
-//     } catch (error) {
-//         console.error('Error saving survey data:', error);
-//         res.status(500).json({ message: 'An error occurred while saving survey data' });
+    try {
+        const insertResult = await collection.insertOne(surveyData);
+        console.log(`Documents successfully inserted.\n`);
+        res.status(200).json({ message: 'Survey data saved successfully' });
+    } catch (error) {
+        console.error('Error saving survey data:', error);
+        res.status(500).json({ message: 'An error occurred while saving survey data' });
 
-//     }
+    }
 
-// });
+});
 
 
 app.listen(port, () => {
